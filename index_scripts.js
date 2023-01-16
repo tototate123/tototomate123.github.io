26aee2312dc8f525b651c90e170c9b14726c8149
tototate123 <116039387+tototate123@users.noreply.github.com>
2023-01-16 17:52:50 +0100

    Update index_scripts.js

--- index_scripts.js
+++ index_scripts.js
@@ -3,7 +3,7 @@ const alerts = {
         "contact": "Add me on Discord! :) \n Louis#7723 \n ------- \n E-Mail: \n louisrost.schule@gmail.com"
     },
     "de": {
-        "contact": "Sende mir eine Freundschaftsanfrage! :) :) \n Louis#7723 \n ------- \n E-Mail: \n louisrost.schule@gmail.com"
+        "contact": "Sende mir eine Freundschaftsanfrage! :) \n Louis#7723 \n ------- \n E-Mail: \n louisrost.schule@gmail.com"
     }
 }
 const buttons = document.querySelectorAll('.round-button');
