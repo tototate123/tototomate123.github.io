379f00b9f868047763e60b4359cd030bde4b8b42
tototate123 <116039387+tototate123@users.noreply.github.com>
2023-01-16 17:49:18 +0100

    Update index_scripts.js

--- index_scripts.js
+++ index_scripts.js
@@ -1,24 +1,27 @@
-// Get all the buttons
+const alerts = {
+    "en": {
+        "contact": "Add me on Discord! :) \n Louis#7723 \n ------- \n E-Mail: \n louisrost.schule@gmail.com"
+    },
+    "de": {
+        "contact": "Fügen Sie mich auf Discord hinzu! :) \n Louis#7723 \n ------- \n E-Mail: \n louisrost.schule@gmail.com"
+    }
+}
 const buttons = document.querySelectorAll('.round-button');
+let currentLanguage = "en";
 
-// Add a click event listener to each button
 buttons.forEach(button => {
     button.addEventListener('click', event => {
-        // Get the text of the button that was clicked
         const buttonText = event.target.id;
-
-        // Use an if-else statement to handle the different button clicks
         if (buttonText === 'about-button') {
             // Code to handle the 'Home' button click
         } else if (buttonText === 'language-switch') {
             // Code to handle the 'About' button click
         } else if (buttonText === 'contact-button') {
-            // Code to handle the 'Contact' button click
-            alert('Add me on Discord! :) \n Louis#7723 \n ------- \n E-Mail: \n louisrost.schule@gmail.com');
-            
+            alert(alerts[currentLanguage].contact);
         }
     });
 });
+
 
 let homeButton = document.getElementById("about-button");
 homeButton.addEventListener("click", function(){
@@ -27,6 +30,7 @@ homeButton.addEventListener("click", function(){
 });
 
 function updateContent(language) {
+    currentLanguage = language; // SEHR WICHTIG FÜR DEN ALERT OBEN!!!!!
     // Load translations from JSON file
     fetch('https://api.npoint.io/a7d9053c972d766856a5')
 
@@ -39,6 +43,7 @@ function updateContent(language) {
             document.querySelector('#about-button').innerText = translations.about;
             document.querySelector('#contact-button').innerText = translations.contact;
             document.querySelector('#language-switch').innerText = translations.lang;
+
             document.querySelector('#portfolio_item1').innerText = translations.portfolio_item1;
         });
 }
